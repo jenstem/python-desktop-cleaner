1192747d5afe9682107c2e4fe38fc48f0561a00a
Jen <jennifer.stemkowski@gmail.com>
2024-03-07 15:45:55 -0500

    Debug folder error

--- python-desktop-cleaner.py
+++ python-desktop-cleaner.py
@@ -7,34 +7,34 @@ from datetime import datetime
 def open_config_file():
     with open('config.json', 'r') as jsonfile:
         data = json.load(jsonfile)
-        return data.get('parameters', {})
+        return data.get('parameters', [])
 
 # Arrange files in the desktop based on the parameters
 def arrange_files(parameters):
     desktop = os.path.expanduser('~/Desktop')
-    logfile = 'python-desktop-cleaner.log'
+    logfile = os.path.join(desktop, 'python-desktop-cleaner.log')
 
     # Create a log or append new entries to the existing log file
     with open(logfile, 'a') as file:
         file.write(f'Start time: {datetime.now()}\n')
 
-# Loop through the parameters
+    # Loop through the parameters
     for parameter in parameters:
-        type = parameter.get('type')
-        folder = parameter.get('folder')
+        type = parameter.get('type', '')
+        folder = parameter.get('folder', '')
         folder_path = os.path.join(desktop, folder)
         os.makedirs(folder_path, exist_ok=True)
 
-# Loop through the files in the desktop
-    for desktop_file in os.listdir(desktop):
-        if desktop_file.endswith(f'.{type}'):
-            file_path = os.path.join(desktop, desktop_file)
-            new_folder = os.path.join(folder_path, desktop_file)
+        # Loop through the files in the desktop
+        for desktop_file in os.listdir(desktop):
+            if desktop_file.endswith(f'.{type}'):
+                file_path = os.path.join(desktop, desktop_file)
+                new_folder = os.path.join(folder_path, desktop_file)
 
-            shutil.move(file_path, new_folder)
+                shutil.move(file_path, new_folder)
 
     with open(logfile, 'a') as file:
-            file.write(f'End time: {datetime.now()}\n')
+        file.write(f'End time: {datetime.now()}\n')
 
 def main():
     parameters = open_config_file()
